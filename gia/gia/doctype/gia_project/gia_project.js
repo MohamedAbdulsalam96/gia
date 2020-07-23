9422c9020e96927ce1612d23ed01ff3cfcc68c20
frappe <frappe@ubuntu.vm>
2020-07-23 22:08:12 +0000

    Inh

--- gia/gia/doctype/gia_project/gia_project.js
+++ gia/gia/doctype/gia_project/gia_project.js
@@ -2,9 +2,35 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('GIA Project', {
-	// refresh: function(frm) {
+	refresh: function(frm) {
+        	cur_frm.set_query("admin1", "gia_project_location", function(doc, cdt, cdn) {
+            		var d = locals[cdt][cdn];
+	            	return{
+	            		filters: [            		    
+	            			['GIA Admin', 'admin_type', '=', 'Governate'],
+	            			['GIA Admin', 'is_group', '=', 1]
+	            		]
+	            	}
+		});
+        	cur_frm.set_query("admin2", "gia_project_location", function(doc, cdt, cdn) {
+            		var d = locals[cdt][cdn];
+	            	return{
+	            		filters: [            		    
+	            			['GIA Admin', 'parent_gia_admin', '=', d.admin1]
+	            		]
+	            	}
+		});
+        	cur_frm.set_query("admin3", "gia_project_location", function(doc, cdt, cdn) {
+            		var d = locals[cdt][cdn];
+	            	return{
+	            		filters: [            		    
+	            			['GIA Admin', 'parent_gia_admin', '=', d.admin2]
+	            		]
+	            	}
+		});
 
-	// }
+
+	},
 	onload: function(frm){
 		cur_frm.set_value("currency", "USD");
 
@@ -25,3 +51,15 @@ frappe.ui.form.on('GIA Project', {
 	}
 
 });
+
+frappe.ui.form.on('GIA Project Location', 'admin1', function(frm, cdt, cdn) {
+	var d = locals[cdt][cdn];
+	d.admin2 = "";
+	d.admin3 = "";
+	frm.refresh_fields();
+})
+frappe.ui.form.on('GIA Project Location', 'admin2', function(frm, cdt, cdn) {
+	var d = locals[cdt][cdn];
+	d.admin3 = "";
+	frm.refresh_fields();
+})
